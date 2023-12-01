d0bae0a38e330d96c7c42258d01375ef28a57429
Geoffrey Kithuku <mutembeikithuku12@gmail.com>
2023-12-01 14:21:09 +0300

    redux toolkit cart

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import CartContainer from "./components/CartContainer";
+import Navbar from "./components/Navbar";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  calculateTotals,
+  getCartItems,
+  isLoading,
+} from "./features/cart/cartSlice";
+
+import { useEffect } from "react";
+import Modal from "./components/Modal";
+
+function App() {
+  const { cartItems } = useSelector((store) => store.cart);
+  const { isOpen } = useSelector((store) => store.modal);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(calculateTotals());
+  }, [cartItems]);
+
+  if (isLoading) {
+    return (
+      <div className="loading">
+        <h1>Loading...</h1>
+      </div>
+    );
+  }
+  return (
+    <main>
+      {isOpen && <Modal />}
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+export default App;
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,46 @@
+import React from "react";
+import { ChevronDown, ChevronUp } from "../icons";
+import { removeItems, increase, decrease } from "../features/cart/cartSlice";
+import { useDispatch } from "react-redux";
+
+const CartItem = ({ id, img, title, price, amount }) => {
+  const dispatch = useDispatch();
+  return (
+    <article className="cart-item">
+      <img src={img} alt="" />
+      <div>
+        <h4>{title}</h4>
+        <h4 className="item-price">${price}</h4>
+        <button
+          onClick={() => dispatch(removeItems(id))}
+          className="remove-btn"
+        >
+          remove
+        </button>
+      </div>
+      <div>
+        <button
+          onClick={() => dispatch(increase({ id }))}
+          className="amount-btn"
+        >
+          <ChevronUp />
+        </button>
+        <p className="amount">{amount}</p>
+        <button
+          onClick={() => {
+            if (amount === 1) {
+              dispatch(removeItems(id))
+              return
+            }
+            dispatch(decrease({ id }));
+          }}
+          className="amount-btn"
+        >
+          <ChevronDown />
+        </button>
+      </div>
+    </article>
+  );
+};
+
+export default CartItem;
